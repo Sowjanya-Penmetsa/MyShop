771f02da0c59df725bce2b757d5bc281573a17f8
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2020-08-06 11:54:14 +0200

    we implemented unity and some config

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿
+using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T>  where T : BaseEntity
+    public class InMemoryRepository<T>  :IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
